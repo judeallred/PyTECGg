9329289cffe2bdf819dde79e2e5a1e3a89ea53b6
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-10-20 15:39:05 +0200

    update observation mappings to enhance RINEX version handling

--- pytecgg/linear_combinations/constants.py
+++ pytecgg/linear_combinations/constants.py
@@ -11,9 +11,31 @@ FREQ_BANDS: dict[str, dict] = {
     "C": {"L1": 1561.098e6, "L5": 1207.14e6},
 }
 
-OBS_MAPPING: dict[str, dict] = {
-    "G": {"phase": {"L1": "L1C", "L2": "L2W"}, "code": {"C1": "C1C", "C2": "C2W"}},
-    "R": {"phase": {"L1": "L1C", "L2": "L2C"}, "code": {"C1": "C1C", "C2": "C2C"}},
-    "E": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
-    "C": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
+# OBS_MAPPING: dict[str, dict] = {
+#     "G": {"phase": {"L1": "L1", "L2": "L2"}, "code": {"C1": "C1", "P2": "P2"}},
+#     "R": {"phase": {"L1": "L1C", "L2": "L2C"}, "code": {"C1": "C1C", "C2": "C2C"}},
+#     "E": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
+#     "C": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
+# }
+
+# OBS_MAPPING: dict[str, dict] = {
+#     "G": {"phase": {"L1": "L1C", "L2": "L2W"}, "code": {"C1": "C1C", "C2": "C2W"}},
+#     "R": {"phase": {"L1": "L1C", "L2": "L2C"}, "code": {"C1": "C1C", "C2": "C2C"}},
+#     "E": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
+#     "C": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
+# }
+
+OBS_MAPPING_BY_VERSION = {
+    "2": {
+        "G": {"phase": {"L1": "L1", "L2": "L2"}, "code": {"C1": "C1", "C2": "P2"}},
+        "R": {"phase": {"L1": "L1", "L2": "L2"}, "code": {"C1": "C1", "C2": "C2"}},
+        "E": {"phase": {"L1": "L1", "L5": "L5"}, "code": {"C1": "C1", "C5": "C5"}},
+        "C": {"phase": {"L1": "L1", "L5": "L5"}, "code": {"C1": "C1", "C5": "C5"}},
+    },
+    "3": {
+        "G": {"phase": {"L1": "L1C", "L2": "L2W"}, "code": {"C1": "C1C", "C2": "C2W"}},
+        "R": {"phase": {"L1": "L1C", "L2": "L2C"}, "code": {"C1": "C1C", "C2": "C2C"}},
+        "E": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
+        "C": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
+    },
 }
