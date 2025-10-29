25b74d5576ce09ef9987efa05f9a7ffaf1430ae5
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-10-29 15:32:24 +0100

    expose calculate_tec

--- pytecgg/tec_calibration/__init__.py
+++ pytecgg/tec_calibration/__init__.py
@@ -1,6 +1,5 @@
 from .arcs import extract_arcs
 from .modip import extract_modip
+from .calibration import calculate_tec
 
-from .calibration import estimate_bias
-
-__all__ = ["extract_arcs", "extract_modip", "estimate_bias"]
+__all__ = ["extract_arcs", "extract_modip", "calculate_tec"]
