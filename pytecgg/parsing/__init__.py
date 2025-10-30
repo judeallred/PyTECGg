780fa2c7b06293a90a37c0b737b8fb5f0cd3b643
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-10-30 14:17:57 +0100

    convert epoch automatically from string to UTC datetime

--- pytecgg/parsing/__init__.py
+++ pytecgg/parsing/__init__.py
@@ -1,3 +1,36 @@
-from ..pytecgg import read_rinex_obs, read_rinex_nav
+import polars as pl
+
+from ..pytecgg import read_rinex_obs as _read_rinex_obs, read_rinex_nav
 
 __all__ = ["read_rinex_obs", "read_rinex_nav"]
+
+
+def read_rinex_obs(path: str):
+    """
+    Parses a RINEX observation file and returns the extracted observation data as a Polars DataFrame.
+
+    Parameters
+    ----------
+    path : str
+        Path to the RINEX observation file.
+
+    Returns
+    -------
+    tuple
+        - pl.DataFrame: DataFrame with columns 'epoch', 'sv', 'observable', 'value'
+        - tuple[float, float, float]: Receiver's position in ECEF coordinates (meters)
+        - str: RINEX version
+
+    Notes
+    -----
+    This is a Python wrapper around the Rust implementation. The 'epoch' column is automatically
+    converted from string to datetime in UTC.
+    """
+    df, rec_pos, rinex_version = _read_rinex_obs(path)
+    df = df.with_columns(
+        pl.col("epoch")
+        .str.strptime(pl.Datetime, format="%Y-%m-%dT%H:%M:%S GPST", strict=False)
+        .dt.replace_time_zone("UTC")
+        .alias("epoch")
+    )
+    return df, rec_pos, rinex_version
