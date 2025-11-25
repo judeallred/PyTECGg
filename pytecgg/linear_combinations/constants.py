83b29c0e1bf7cb10d652de547a469a2768032970
Vincenzo <vincenzo.ventriglia@outlook.com>
2025-11-25 17:42:29 +0100

    WIP - frequencies and channels

--- pytecgg/linear_combinations/constants.py
+++ pytecgg/linear_combinations/constants.py
@@ -7,23 +7,9 @@ FREQ_BANDS: dict[str, dict] = {
         "L1": lambda n: (1602 + n * 0.5625) * 1e6,
         "L2": lambda n: (1246 + n * 0.4375) * 1e6,
     },
-    "E": {"L1": 1575.42e6, "L5": 1176.45e6},
+    "E": {"L1": 1575.42e6, "L2": 1176.45e6},
     "C": {"L1": 1561.098e6, "L5": 1207.14e6},
 }
-
-# OBS_MAPPING: dict[str, dict] = {
-#     "G": {"phase": {"L1": "L1", "L2": "L2"}, "code": {"C1": "C1", "P2": "P2"}},
-#     "R": {"phase": {"L1": "L1C", "L2": "L2C"}, "code": {"C1": "C1C", "C2": "C2C"}},
-#     "E": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
-#     "C": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
-# }
-
-# OBS_MAPPING: dict[str, dict] = {
-#     "G": {"phase": {"L1": "L1C", "L2": "L2W"}, "code": {"C1": "C1C", "C2": "C2W"}},
-#     "R": {"phase": {"L1": "L1C", "L2": "L2C"}, "code": {"C1": "C1C", "C2": "C2C"}},
-#     "E": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
-#     "C": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
-# }
 
 OBS_MAPPING_BY_VERSION = {
     "2": {
@@ -35,7 +21,7 @@ OBS_MAPPING_BY_VERSION = {
     "3": {
         "G": {"phase": {"L1": "L1C", "L2": "L2W"}, "code": {"C1": "C1C", "C2": "C2W"}},
         "R": {"phase": {"L1": "L1C", "L2": "L2C"}, "code": {"C1": "C1C", "C2": "C2C"}},
-        "E": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
-        "C": {"phase": {"L1": "L1X", "L5": "L5X"}, "code": {"C1": "C1X", "C5": "C5X"}},
+        "E": {"phase": {"L1": "L1C", "L2": "L5Q"}, "code": {"C1": "C1C", "C5": "C5Q"}},
+        "C": {"phase": {"L1": "L2I", "L2": "L6I"}, "code": {"C1": "C2I", "C6": "C6I"}},
     },
 }
